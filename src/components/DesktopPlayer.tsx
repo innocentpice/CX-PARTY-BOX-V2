60f24240e5a6bd36ae36e6f6f8365516e8a9cb57
Chanachai <chanachai.devper@gmail.com>
2022-12-25 20:35:21 +0700

    update

--- src/components/DesktopPlayer.tsx
+++ src/components/DesktopPlayer.tsx
@@ -143,7 +143,7 @@ const bufferLoader = async (musicURL: string, start: number, chunkSize: number, 
   const fetcher = async (musicURL: string, start: number, chunkSize: number) => {
     if (start > maxChunkSize) return Promise.resolve(Buffer.alloc(0));
     const rStart = start;
-    const rEnd = start + chunkSize;
+    const rEnd = start + chunkSize > maxChunkSize ? maxChunkSize : start + chunkSize;
     return fetch(musicURL, {
       signal: bufferAbotController?.signal,
       headers: { Range: `bytes=${rStart}-${rEnd}` },
